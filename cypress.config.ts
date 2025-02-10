b6b0949e2b5aa0654192f9e6b05d90f1febdb1b4
Perseverance Munga <percymunga02@gmail.com>
2025-02-10 22:49:46 +0000

    change path of test file in cypress configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     supportFile: false,
-    specPattern: "src/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "src/cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
   },
 })
